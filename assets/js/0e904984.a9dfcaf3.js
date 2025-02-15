c56cf78dc9e525c28d7582e16d528c5c5ef0af16
JulioC354R <julyocesarplay@gmail.com>
2025-02-15 19:23:48 -0300

    Deploy website - based on 1edcf254606538f842dee9f1476cdcd1503c0036

---
+++ assets/js/0e904984.a9dfcaf3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[7316],{6002:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/JulioHerculano/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.0c8ceb6b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_page=self.webpackChunkmy_page||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/JulioHerculano/blog","blogTitle":"Blog","authorsListPath":"/JulioHerculano/blog/authors"}')}}]);
